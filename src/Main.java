5c68909e3b0c7ade2268683ccd1e7b896852677b
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-03 12:55:20 +0400

    Change name variable

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int starting_Personal_Account = 500; // начальный счет
-        int refill = 500; // сумма пополнения
+        int startingPersonalAccount = 100; // начальный счет
+        int refill = 1100; // сумма пополнения
         int rate = 100; // коэффициэнт 100р. = bonus_Point
-        int bonus_Point = 1;  // бонусные рубли
-        int deposit_Amount = 1000; // минимальный платеж, с которого идет бонус
+        int bonusPoint = 1;  // бонусные рубли
+        int depositAmount = 1000; // минимальный платеж, с которого идет бонус
 
-        int bonus = refill >= deposit_Amount ? refill * bonus_Point / rate : 0;
-        int final_Personal_Account = refill >= deposit_Amount ? starting_Personal_Account + refill + bonus : starting_Personal_Account + refill;
+        int bonus = refill >= depositAmount ? refill * bonusPoint / rate : 0;
+        int finalPersonalAccount = refill >= depositAmount ? startingPersonalAccount + refill + bonus : startingPersonalAccount + refill;
 
-        System.out.println("Итоговый счёт: " + final_Personal_Account);
+        System.out.println("Итоговый счёт: " + finalPersonalAccount);
         System.out.println("В том числе, бонусных рублей: " + bonus);
     }
 }
